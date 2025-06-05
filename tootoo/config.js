6475f7f7cf543d7f140ebe4e9ecac13f6e0a8fc1
Theo Armour <t.armour@gmail.com>
2025-06-04 21:18:11 -0700

    Reorganize CONFIG object properties for improved readability

--- tootoo/config.js
+++ tootoo/config.js
@@ -4,13 +4,17 @@ const CONFIG = {
     repo: "2025",
     branch: "main",
 
-    menuTitle: "Theo 2025",
-    menuTitleEdit: "T2025 Edit",
+    filterFolders: [ "archive", "tootoo"],
+    ignoreFiles: [ "404.html", "favicon.ico", "index.html", "readme.html", "test.md"],
 
     defaultFile: "README.md",
     defaultFileEdit: "@@README.md",
 
-    filterFolders: [ "archive", "tootoo"],
-    ignoreFiles: [ "404.html", "favicon.ico", "index.html", "readme.html", "test.md"],
+    menuTitle: "Theo 2025",
+    menuTitleEdit: "T2025 Edit",
+
+    /* 0 to 360 10=red 120=green 240=blue */
+    mainHue: 120,
+    baseMenuWidth: 15,
 
 }
